f0d4ea30f1cbc8610fee93672aa4c288b6227fa3
EmanuelCN <emanuelghub@gmail.com>
2024-06-03 20:19:00 +0000

    fog: Override cgroups.json & task_profiles.json * In order to allow uclamp kernels to place tasks correctly

--- device.mk
+++ device.mk
@@ -112,6 +112,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Configs File System
 PRODUCT_PACKAGES += \
     fs_config_files
