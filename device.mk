4de98685a117b3901588b403b149c51c6e7e0f5d
Guidix <guidixm@gmail.com>
2024-10-18 10:38:58 +0300

    fog: Drop Telephony Package
    Already Included in vendor/yaap
    
    [alternoegraha]: kept xiaomi-telephony-stub
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -440,17 +440,9 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext \
     xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext \
     xiaomi-telephony-stub
 
 PRODUCT_COPY_FILES += \
