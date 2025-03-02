d28c8ab498d10702b83bffaa1d44af7b127c7490
alternoegraha <noegrahachan@gmail.com>
2025-03-02 07:56:30 +0700

    fog: add RemovePackages
    - drop LineageOS' AudioFX and LMOFreeform
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -375,6 +375,10 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor \
     libvndfwk_detect_jni.qti.vendor
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
