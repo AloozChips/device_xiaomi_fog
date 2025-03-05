96553b7373b017fcd11b433ff02ba7b0d79e8ce0
alternoegraha <noegrahachan@gmail.com>
2025-03-06 04:33:07 +0700

    fog: yaap bringup
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_fog.mk
+    $(LOCAL_DIR)/yaap_fog.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,5 +7,5 @@ git clone -b refresh https://github.com/alternoegraha/vendor_xiaomi_fog vendor/x
 # Clone prebuilt kernel
 git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
 
-# Clone PixelOS hardware/xiaomi
-git clone -b fifteen https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
+# Clone YAAP hardware/xiaomi
+git clone -b fifteen https://github.com/yaap/hardware_xiaomi hardware/xiaomi
