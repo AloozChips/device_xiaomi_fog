94077a4d77e2690208a3c05fa884a6811bc5a4f8
alternoegraha <noegrahachan@gmail.com>
2025-08-18 06:07:41 +0000

    fog: @AxionAOSP time
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- lineage_fog.mk
+++ lineage_fog.mk
@@ -15,6 +15,17 @@ TARGET_BOOT_ANIMATION_RES := 720
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
+# AxionAOSP specific flags
+TARGET_PREBUILT_BCR := false
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDES_LOS_PREBUILTS := true
+AXION_CAMERA_REAR_INFO := 50,5
+AXION_CAMERA_FRONT_INFO := 5
+AXION_MAINTAINER := alternoegraha
+AXION_PROCESSOR := Snapdragon_680_(6nm)
+AXION_CPU_BIG_CORES := 4,5,6,7
+AXION_CPU_SMALL_CORES := 0,1,2,3
+
 PRODUCT_NAME := lineage_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
