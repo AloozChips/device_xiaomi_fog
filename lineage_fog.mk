1ef64c156a5e9011767d41d05707481506179382
alterneograha <noegrahachan@gmail.com>
2025-10-17 22:14:57 +0000

    fog: Update build flags and drop GrapheneCamera
    - Now I can ship LineageOS' Aperture camera
    - do not install AudioFX by default
    - disable `TORCH_STR_SUPPORTED`, our device doesn't support it
    - trying to disable non-functional Quick Tap with `TARGET_SUPPORTS_QUICK_TAP := false` (maybe not work, but anyway)
    
    Signed-off-by: alterneograha <noegrahachan@gmail.com>

--- lineage_fog.mk
+++ lineage_fog.mk
@@ -11,25 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_QUICK_TAP := false
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
-
-# Inherit additional prebuilts
-$(call inherit-product-if-exists, vendor/additional-prebuilts/extras.mk)
-# Include GrapheneOS Camera
-TARGET_INCLUDE_GRAPHENE_CAMERA := true
 
 # AxionAOSP specific flags
 TARGET_PREBUILT_BCR := false
 TARGET_ENABLE_BLUR := true
 TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_EXCLUDES_AUDIOFX := true
+PRODUCT_NO_CAMERA := false
 AXION_CAMERA_REAR_INFO := 50,5
 AXION_CAMERA_FRONT_INFO := 5
 AXION_MAINTAINER := alternoegraha
 AXION_PROCESSOR := Snapdragon_680_(6nm)
 AXION_CPU_BIG_CORES := 4,5,6,7
 AXION_CPU_SMALL_CORES := 0,1,2,3
+TORCH_STR_SUPPORTED := false
 
 PRODUCT_NAME := lineage_fog
 PRODUCT_DEVICE := fog
